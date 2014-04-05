6cb6d46e73edb3c79e2bc1ad6e8a79ebddd5ca6d
awsmdavid <awsmdavid@gmail.com>
2014-04-05 18:17:33 -0400

    first push

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var end = new Date('02/19/2015 10:1 AM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById('countdowndays').innerHTML = 'MARRIED!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdowndays').innerHTML = days;
+        document.getElementById('countdownhrs').innerHTML = hours;
+        document.getElementById('countdownmins').innerHTML = minutes;
+        document.getElementById('countdownsecs').innerHTML = seconds;
+    }
+
+    timer = setInterval(showRemaining, 1000);
